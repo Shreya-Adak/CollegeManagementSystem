3c7247cf6d0159b56219e91a50d0000cb61774b8
Shreya-Adak <107557940+Shreya-Adak@users.noreply.github.com>
2024-07-22 03:09:49 +0530

    CollegeManagementSystem

---
+++ src/test/java/com/example/codingshuttle/collegemanagementsystem/CollegeManagementSystem/CollegeManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codingshuttle.collegemanagementsystem.CollegeManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollegeManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
